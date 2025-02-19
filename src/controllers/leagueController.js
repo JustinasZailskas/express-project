fefabd2176725b08121cce6383f332676d0f54c2
JustinasZailskas <justinaszailskas@gmail.com>
2025-02-19 22:20:25 +0200

    try catch

--- src/controllers/leagueController.js
+++ src/controllers/leagueController.js
@@ -2,12 +2,17 @@ const League = require("../models/league");
 const leagueService = require("../services/leagueService");
 
 exports.getAllLeagues = (req, res) => {
-  leagueService.getAllLeagues().then((leagueItems) => {
-    if (!leagueItems) {
-      throw new Error("Elementai nerasti", 404);
-    }
-    console.log(leagueItems);
-    res.json(leagueItems);
-  });
-  console.log("Bet kas");
+  try {
+    leagueService.getAllLeagues().then((leagueItems) => {
+      if (!leagueItems) {
+        throw new Error("Elementai nerasti", 404);
+      }
+      console.log(leagueItems);
+      res.json(leagueItems);
+    });
+    console.log("Bet kas");
+  } catch (error) {
+    console.log("Klaida: ", error.message);
+    throw error;
+  }
 };
